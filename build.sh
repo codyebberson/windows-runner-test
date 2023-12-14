d8e10afe7a51d023e18e57d453699974a00de1fa
Cody Ebberson <cody@ebberson.com>
2023-12-14 14:01:21 -0800

    Shawl test

--- build.sh
+++ build.sh
@@ -11,6 +11,17 @@ java --version
 node --version
 npm --version
 
+# Make dist directory
+mkdir -p dist
+
+# Download Shawl
+wget https://github.com/mtkennerly/shawl/releases/download/v1.4.0/shawl-v1.4.0-win64.zip
+unzip shawl-v1.4.0-win64.zip
+mv shawl-v1.4.0-win64.exe dist
+wget https://github.com/mtkennerly/shawl/releases/download/v1.4.0/shawl-v1.4.0-legal.zip
+unzip shawl-v1.4.0-legal.zip
+mv shawl-v1.4.0-legal.txt dist
+
 # Generate a checksum
 echo "Hello world" > hello.txt
 sha256sum hello.txt > hello.txt.sha256
